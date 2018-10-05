a8c1060bbe45245ee29672cb94920eb949017a2b
Max Beech <mb660@exeter.ac.uk>
2018-10-05 17:45:15 +0100

    Confirmed working on iOS

--- src/views/example/index-screen/index-screen.tsx
+++ src/views/example/index-screen/index-screen.tsx
@@ -1,61 +1,111 @@
-import * as React from "react"
-import { View, ViewStyle, TextStyle, StatusBar } from "react-native"
-import { NavigationScreenProps } from "react-navigation"
-import { Button } from "../../shared/button"
-import { Screen } from "../../shared/screen"
-import { Wallpaper } from "../../shared/wallpaper"
-import { color, spacing } from "../../../theme"
-import { Text } from "../../shared/text"
-import { ToDoStore } from "../../../models/to-do-list-store/to-do-list-store"
-import { inject } from "mobx-react"
+import React, { Component } from "react"
+import {
+  StyleSheet,
+  Text,
+  Image,
+  Dimensions,
+  ScrollView,
+  ListView,
+  Button,
+  View,
+} from "react-native"
+import {
+  TouchThroughView,
+  TouchThroughWrapper,
+} from "react-native-touch-through-view"
+import { IndexScreenProps } from "./index-screen-orig"
 
-const FULL: ViewStyle = { flex: 1 }
-const CONTAINER: ViewStyle = {
-  backgroundColor: color.transparent,
-  paddingHorizontal: spacing[4],
-  paddingTop: spacing[6],
-}
-const HEADER: TextStyle = {
-  paddingTop: spacing[3],
-  paddingBottom: spacing[4] + spacing[1],
-  paddingHorizontal: 0,
-  textAlign: "center",
-}
-
-export interface IndexScreenProps extends NavigationScreenProps<{}> {
-  toDoStore: ToDoStore
-}
-
-@inject("toDoStore")
 export class IndexScreen extends React.Component<IndexScreenProps, {}> {
-  toDoList = () => {
-    this.props.toDoStore.loadTodos()
-    this.props.navigation.navigate("toDoList")
-  }
-
-  drawerScreen = () => {
-    this.props.navigation.navigate("drawerStack")
-  }
-
   render() {
     return (
-      <View style={FULL}>
-        <StatusBar barStyle="light-content" />
-        <Wallpaper />
-        <Screen
-          style={CONTAINER}
-          backgroundColor={color.transparent}
-          preset="scrollStack"
-        >
-          <Text preset="header" text={"Examples"} style={HEADER} />
-          <Button preset="link" onPress={this.toDoList} text={"ToDoList"} />
+      <View style={styles.container}>
+        <View style={styles.artistInfo}>
+          <View style={styles.albumImageWrapper}>
+            <Image source={require("./album.jpg")} style={styles.albumImage} />
+          </View>
           <Button
-            preset="link"
-            onPress={this.drawerScreen}
-            text={"DrawerScreen"}
+            title="Play"
+            onPress={() => {
+              alert("Touched!")
+            }}
           />
-        </Screen>
+        </View>
+        <TouchThroughWrapper style={styles.scrollWrapper}>
+          <ListView
+            style={styles.scroller}
+            dataSource={dataSource}
+            renderHeader={() => (
+              <TouchThroughView style={styles.touchThroughView} />
+            )}
+            renderRow={rowData => {
+              return <Text style={styles.itemRow}>{rowData}</Text>
+            }}
+          />
+        </TouchThroughWrapper>
       </View>
     )
   }
 }
+
+const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+const rows = []
+
+let i = 0
+while (i < 1000) {
+  rows.push(`Item ${i}`)
+  i++
+}
+
+const dataSource = ds.cloneWithRows(rows)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eee",
+    paddingTop: 20,
+  },
+  button: {
+    marginTop: 20,
+  },
+  albumImage: {
+    width: 300,
+    height: 300,
+  },
+  albumImageWrapper: {
+    marginTop: 20,
+    marginBottom: 10,
+    padding: 10,
+    width: 320,
+    height: 320,
+    alignSelf: "center",
+    backgroundColor: "white",
+    shadowColor: "#ccc",
+    shadowOpacity: 1,
+    shadowRadius: 5,
+    shadowOffset: { width: 0, height: 0 },
+  },
+  artistInfo: {
+    position: "absolute",
+    top: 20,
+    left: 0,
+    right: 0,
+    height: 400,
+    zIndex: 0,
+  },
+  scroller: {
+    zIndex: 1,
+  },
+  scrollWrapper: {
+    flex: 1,
+  },
+  touchThroughView: {
+    height: 400,
+    flex: 1,
+  },
+  itemRow: {
+    backgroundColor: "#ddd",
+    padding: 20,
+    borderBottomWidth: 5,
+    borderBottomColor: "#000",
+  },
+})
